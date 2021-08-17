393247ad50c6cec5da1559b54d5d58207e3c4320
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-08-17 05:44:33 -0400

    Finished setting up react and sass. Getting started on styling the calculator.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="calculator">
+        <h1>Bill</h1>
+
+        <h2>Select Tip %</h2>
+        <ul>
+          <li>5%</li>
+          <li>10%</li>
+          <li>15%</li>
+          <li>25%</li>
+          <li>50%</li>
+          <li>Custom</li>
+        </ul>
+
+        <div id="num-people">
+          <label for="num-people__input">Number of People</label>
+          <input type="text" id="num-people__input" placeholder="eg. 3" />
+        </div>
+
+        <div class="tip-amount">
+          <h3>Tip Amount</h3>
+          <p>/ person</p>
+        </div>
+
+        <div class="total">
+          <h3>Total</h3>
+          <p>/ person</p>
+        </div>
+
+        <button id="reset-btn">Reset</button>
+      </div>
     </div>
   );
 }
